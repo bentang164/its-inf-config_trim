6476cb27348515e81ef30a1b6178fc131121d052
Ben Tang <40529554+bentang164@users.noreply.github.com>
2023-11-10 15:31:43 -0600

    Implemented more stuff

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Runner {
+    private final List<String> VALID_ARGS = Arrays.asList(new String[]{"--edit-config", "--delete-config", "--version", "--help"});
+    private final String VERSION_BUILD_DAY_CHAR = "Fri";
+    private final String VERSION_BUILD_MONTH = "Nov";
+    private final int VERSION_BUILD_DAY_NUMERIC = 10;
+    private final int VERSION_BUILD_YEAR = 23;
+    private final int VERSION_MAJOR = 3;
+    private final int VERSION_MINOR = 0;
+
     private boolean editConfig, deleteConfig;
 
     public Runner() {
@@ -7,12 +18,41 @@ public class Runner {
     }
 
     private void validateArgs(String[] args) {
-        for (String arg : args) {
-            switch (arg) {
-                case "--edit-config" : editConfig = true;
-                case "--delete-config" : deleteConfig = true;
-                default : System.out.println("[warn] Unrecognized argument '" + arg + "', ignoring");
-            }
+        if (args.length < 1) {
+            return;
+        }
+
+        if (args.length > 1) {
+            System.out.println("[warn] Invalid arguments. Expected: 1, got: " + args.length + ". Ignoring extra arguments");
+        }
+
+        String arg = args[0];
+
+        if (!VALID_ARGS.contains(arg)) {
+            System.out.println("[warn] Invalid argument '" + arg + "'. Ignoring.");
+            return;
+        }
+
+        if (arg.equals("--edit-config")) {
+            editConfig = true;
+            return;
+        }
+
+        if (arg.equals("--delete-config")) {
+            deleteConfig = true;
+            return;
+        }
+
+        if (arg.equals("--version")) {
+            System.out.println("Configuration Trimmer Software, Version " + VERSION_MAJOR + "." + VERSION_MINOR + 
+                               "\nCompiled " + VERSION_BUILD_DAY_CHAR + " " + VERSION_BUILD_DAY_NUMERIC + "-" + VERSION_BUILD_MONTH + "-" + VERSION_BUILD_YEAR + " by btang");
+            
+            System.exit(0);
+        }
+
+        if (arg.equals("--help")) {
+            System.out.println("usage: java -jar Trim.jar [--edit-config] [--delete-config] [--version] [--help]");
+            System.exit(0);
         }
     }
     
--- src/SelfConfigHelper.java
+++ src/SelfConfigHelper.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 
 public class SelfConfigHelper {
     private final String CONFIG_PATH = System.getProperty("user.home") + "/.trimHelper_config"; 
+    private boolean editConfig;
     public List<String> commandsToExclude;
 
     public SelfConfigHelper() {
+        editConfig = false;
         commandsToExclude = new ArrayList<>();
     }
 
     private boolean checkForConfig() {
-        new File(CONFIG_PATH).delete();     // debug purposes, delete before pushing to prod...
-
         File config = new File(CONFIG_PATH);
 
         if (config.exists() && !config.isDirectory()) {
@@ -28,9 +28,9 @@ public class SelfConfigHelper {
     }
 
     private void createConfigFile() {
-        System.out.println("[warn] No config file found. Will create one at " + CONFIG_PATH);
+        System.out.println("[info] No config file found. Will create one at " + CONFIG_PATH);
         System.out.println("[info] This config file will contain all commands that will be removed.");
-        System.out.println("[info] Enter configuration commands, one per line.  End by passing EXIT.\n");
+        System.out.println("\nEnter configuration commands, one per line.  End by passing EXIT.");
 
         Scanner input = new Scanner(System.in);
 
@@ -46,7 +46,11 @@ public class SelfConfigHelper {
                     commandsToExclude.add(command);
                 }
             } else {
-                System.out.println("[warn] Command must not be of zero length.");
+                for (int i = 0; i < "TrimHelper(config)#".length(); i++) {
+                    System.out.print(" ");
+                }
+                System.out.println("^");
+                System.out.println("% Invalid input detected at '^' marker. Command must not be of zero length.\n");
             }
 
             if (command.toLowerCase().equals("exit")) {
@@ -72,6 +76,7 @@ public class SelfConfigHelper {
             FileWriter configFile = new FileWriter(CONFIG_PATH);
 
             configFile.write("// TrimConfig configuration file generated " + ZonedDateTime.now() + ".\n");
+            configFile.write("// Commands listed under this line will be removed from the input running configuration.\n");
 
             for (String command : commandsToExclude) {
                 configFile.write(command + "\n");
@@ -103,10 +108,96 @@ public class SelfConfigHelper {
             e.printStackTrace();
             System.exit(-1);
         }   
+
+        if (editConfig) {
+            editConfig();
+        }
+    }
+
+    private void editConfig() {
+        List<String> commandsToRemove = new ArrayList<>();
+
+        System.out.println("Building configuration...\n");
+
+        File config = new File(CONFIG_PATH);
+
+        System.out.println("Current configuration : " + config.length() + " bytes\n!");
+        for (String command : commandsToExclude) {
+            if (!command.startsWith("//")) {
+                System.out.println(" " + command);
+            }
+        }
+
+        System.out.println("end\n");
+
+        System.out.println("Enter configuration commands to remove, one per line. Prefix with 'no'. End by passing EXIT.");
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("TrimHelper(config-rem)#");
+
+            String command = input.nextLine();
+
+            if (command.length() >= 1) {
+                if (command.toLowerCase().equals("exit")) {
+                    break;
+                } else {
+                    if (!command.toLowerCase().startsWith("no")) {
+                        for (int i = 0; i < "TrimHelper(config-rem)#".length(); i++) {
+                            System.out.print(" ");
+                        }
+                        System.out.println("^");
+                        System.out.println("% Invalid input detected at '^' marker. Command must be prefixed with 'no'.\n");
+                    } else {
+                        commandsToRemove.add(command.substring(3));
+                    }
+                }
+            } else {
+                for (int i = 0; i < "TrimHelper(config-rem)#".length(); i++) {
+                    System.out.print(" ");
+                }
+                System.out.println("^");
+                System.out.println("% Invalid input detected at '^' marker. Command must not be of zero length.\n");
+            }
+
+            if (command.toLowerCase().equals("exit")) {
+                break;
+            }
+        }
+
+        input.close();
+
+        if (commandsToRemove.size() < 1) {
+            System.out.println("[warn] No commands specified to remove, continuing without modifying config.");
+        } else {
+            for (int i = 0; i < commandsToExclude.size() - 1; i++) {
+                for (String command : commandsToRemove) {
+                    if (commandsToExclude.get(i).equals(command)) {
+                        commandsToExclude.remove(i);
+                    }
+                }
+            }
+
+            new File(CONFIG_PATH).delete();
+            writeConfigFile();
+        }
     }
 
     public void exec(boolean editConfig, boolean deleteConfig) {
+        this.editConfig = editConfig;
+
+        if (deleteConfig) {
+            new File(CONFIG_PATH).delete();
+            System.out.println("[info] Config file deleted, on next run will prompt to recreate.");
+            System.exit(0);
+        }
+        
         if (!checkForConfig()) {
+            if (editConfig) {
+                System.out.println("[warn] --edit-config was passed, but no config file previously existed. Ignoring argument.");
+                editConfig = false;
+            }
             createConfigFile();
         } else {
             readConfig();
