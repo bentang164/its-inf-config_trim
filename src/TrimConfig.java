4f93d182b8b3b0a6914361ad4f0e830776b5f19b
Ben Tang <40529554+bentang164@users.noreply.github.com>
2023-11-10 12:43:41 -0600

    Finished pseudocode

--- src/TrimConfig.java
+++ src/TrimConfig.java
@@ -1,28 +1,37 @@
 public class TrimConfig {
     /**
-     * Determine if program is being run on Mac or Windows. 
-     *  
-     * Test if config file already exists at:
-     *      ~/Library/Application Support/TrimConfig/config.text    (mac)
-     *      %localappdata%\TrimConfig\config.text                   (win)
+     * Test if config file already exists at: ~/Library/Application Support/TrimConfig/config.text
      *      If yes: 
-     *          Read config file to load in set of default commands.
+     *          Read config file to load in set of default commands and input in list.
+     *          Prompt user to input building default VLAN.
+     *          Add 'switchport access vlan [building VLAN]' to list of default commands.
      *      Else: 
-     *          Prompt user to input default commands to exclude.
+     *          Prompt user to input default commands to exclude and store in a temporary list.
+     *          If the user's input contains 'spanning-tree portfast':
+     *              Add 'spanning-tree portfast' to the list
+     *              Add 'spanning-tree portfast edge' to the list
+     *          Write header data to the location above.
      *          Write this data to the location above.
      * 
      *      Prompt user for location of file to trim.
      *      
      * Read in the specified file to trim.
      * 
-     *      For each line in the file:
+     *      While the current line does not contain 'interface vlan':
+     *          If the update marker is true:
+     *              Advance the current line to the next line
+     *          
      *          If the current line contains 'interface':
-     *              Set per-interface configuration flag to true
-     *              Add the current line to the map
-     *              Set the current line as the interface marker
+     *              Update the interface marker to the current line
      *              While the current line does not contain 'interface':
-     *                  Advance the current line to the next line
-     *                  If the current line is not in the list of commands to exclude:
-     *                      Add the current line to the map at the interface marker
-     *  */            
+     *                  Read in the next line
+     *                  For each command in the list of commands to exclude:
+     *                      If the current line matches on a command:
+     *                          Update the exclude marker to true 
+     *                  If the exclude marker is false:
+     *                      Add the current line into a list of commands to keep
+     *              
+     *              Add the interface marker as the key into the map, and the list of commands as the value
+     *              Set the update marker to false so the line is not advanced
+     *  */  
 }
