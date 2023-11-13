d89e311e5a46c757bf1445e00723fe9d9c3d60ff
Ben Tang <40529554+bentang164@users.noreply.github.com>
2023-11-13 13:00:50 -0600

    Added startup message

--- src/Runner.java
+++ src/Runner.java
@@ -8,12 +8,11 @@ import java.util.regex.Pattern;
 public class Runner {
     private final List<String> VALID_ARGS = Arrays.asList(new String[]{"--edit-config", "--delete-config", "--show-config", "--version", "--help"});
     private final Pattern QUOTATION_MARKS = Pattern.compile("[\"'\u2018\u2019\u201c\u201d]");   // https://stackoverflow.com/a/35534669
-    private final String VERSION_BUILD_DAY_CHAR = "Fri";
+    private final String VERSION_BUILD_DAY_CHAR = "Mon";
     private final String VERSION_BUILD_MONTH = "Nov";
-    private final int VERSION_BUILD_DAY_NUMERIC = 10;
+    private final String VERSION = "3.0.0prd1";
+    private final int VERSION_BUILD_DAY_NUMERIC = 13;
     private final int VERSION_BUILD_YEAR = 23;
-    private final int VERSION_MAJOR = 3;
-    private final int VERSION_MINOR = 0;
     
     private String userFilePath;
 
@@ -64,9 +63,7 @@ public class Runner {
         }
 
         if (arg.equals("--version")) {
-            System.out.println("Configuration Trimmer Software, Version " + VERSION_MAJOR + "." + VERSION_MINOR + 
-                               "\nCompiled " + VERSION_BUILD_DAY_CHAR + " " + VERSION_BUILD_DAY_NUMERIC + "-" + VERSION_BUILD_MONTH + "-" + VERSION_BUILD_YEAR + " by btang");
-            
+            printStart();
             printExit();
             System.exit(0);
         }
@@ -132,12 +129,29 @@ public class Runner {
         fileName = cleanPath.substring(0, cleanPath.lastIndexOf(".")) + "_trimmed" + fileExtension;
     }
 
+    private void printStart() {
+        System.out.println("\nConfiguration Trimmer Software, Version " + VERSION + ", RELEASE SOFTWARE (fc1)");
+        System.out.println("Technical Support: https://github.com/bentang164/its-inf-config_trim#readme");
+        System.out.println("Compiled " + VERSION_BUILD_DAY_CHAR + " " + VERSION_BUILD_DAY_NUMERIC + "-" + VERSION_BUILD_MONTH + "-" +
+        VERSION_BUILD_YEAR + " by btang");
+        System.out.println("\n\n" +
+        "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS " +
+        "OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY " + 
+        "AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR " + 
+        "CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL " +
+        "DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, " +
+        "DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN " +
+        "CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF " +
+        "THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n");
+    }
+
     public static void printExit() {
         System.out.println("[info] Exited Configuration Trimmer session at " + ZonedDateTime.now());
     }
 
     public static void main(String[] args) {
         Runner runner = new Runner();
+        runner.printStart();
         runner.validateArgs(args);
         runner.getInfo();
 
